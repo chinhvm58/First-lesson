e32852530f3fe91ed030b99f8a942b6c92c94f09
chinhvm58 <boyhaloi1995@gmail.com>
2015-08-18 16:19:16 +0700

    add

---
+++ Beginner/Assets/ZoombieAnimator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+	
+public class ZoombieAnimator : MonoBehaviour {
+	private SpriteRenderer spriteRenderer;
+	public Sprite[] sprites;
+	public float framesPerSecond;
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<Renderer>() as SpriteRenderer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+		index = index % sprites.Length;
+		spriteRenderer.sprite = sprites[ index ];
+	}
+}
